3be6f65efccdea39c4def4f51e739c6c243a0ef0
BrunoBelarminoNog <brunobelarm.nog@gmail.com>
2021-06-09 20:40:48 -0300

    feat: aside renderizando em rotas autenticadas

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
+import { useContext } from "react";
 import Header from "./components/Header";
 import Routes from "./routes";
 import { GlobalStyles } from "./styles/global";
+import {UserContext} from './providers/User'
+import Aside from "./components/Aside";
 
 function App() {
+  const {authenticated} = useContext(UserContext);
+  
   return (
     <>
       <Header />
+      {
+        authenticated && <Aside />
+      }
       <Routes />
       <GlobalStyles />
     </>
