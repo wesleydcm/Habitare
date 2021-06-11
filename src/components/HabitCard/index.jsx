bc694c67a3fa1af40688b1f982c6bc5cc24a94ab
Guilherme Lemes de Freitas <guilemes300@gmail.com>
2021-06-11 11:38:47 -0300

    feature: new modal for checkin

--- src/components/HabitCard/index.jsx
+++ src/components/HabitCard/index.jsx
@@ -1,13 +1,22 @@
 import React, { useEffect, useState } from "react";
 import Lottie from "react-lottie";
-import {categoryFormat, difficultyFormat} from '../../utils/format'
-import { CardContainer, HabitInfo, ImageContainer, InfoHowMuchAchieved } from "./styles";
+import { categoryFormat, difficultyFormat } from "../../utils/format";
+import ModalCheckin from "../ModalCheckin";
+import {
+  CardContainer,
+  HabitInfo,
+  ImageContainer,
+  InfoHowMuchAchieved,
+} from "./styles";
 
-
-const HabitCard = ({habit}) => {
+const HabitCard = ({ habit }) => {
+  const [isModalVisible, setIsModalVisible] = useState(false);
   const [habitFormatted, setHabitFormatted] = useState({});
-    const [paused, setPaused] = useState(true)
-  
+  const [paused, setPaused] = useState(true);
+
+  const showModal = () => {
+    setIsModalVisible(true);
+  };
 
   useEffect(() => {
     const difficultyFormatted = difficultyFormat(habit.difficulty);
@@ -23,26 +32,42 @@ const HabitCard = ({habit}) => {
     // eslint-disable-next-line
   }, []);
 
-
   return (
-    <CardContainer category={habitFormatted.category} onMouseEnter={() => setPaused(false)} onMouseLeave={() => setPaused(true)}>
-      <HabitInfo>
-        <h2>{habit.title}</h2>
-        <h3>
-          {habitFormatted.categoryFormatted?.icon}
-          {habitFormatted.categoryFormatted?.title}
-        </h3>
-        <span>
-          {habitFormatted.difficultyFormatted?.icons}
-        </span>
-        <InfoHowMuchAchieved howMuchAchieved={habitFormatted.how_much_achieved}>
-          <div></div>
-        </InfoHowMuchAchieved>
-      </HabitInfo>
-      <ImageContainer category={habitFormatted.category} className="lottie">
-          { habitFormatted.categoryFormatted && <Lottie options={habitFormatted.categoryFormatted?.image} isPaused={paused}/> }
-      </ImageContainer>
-    </CardContainer>
+    <>
+      <CardContainer
+        category={habitFormatted.category}
+        onMouseEnter={() => setPaused(false)}
+        onMouseLeave={() => setPaused(true)}
+      >
+        <HabitInfo onClick={showModal}>
+          <h2>{habit.title}</h2>
+          <h3>
+            {habitFormatted.categoryFormatted?.icon}
+            {habitFormatted.categoryFormatted?.title}
+          </h3>
+          <span>{habitFormatted.difficultyFormatted?.icons}</span>
+          <InfoHowMuchAchieved
+            howMuchAchieved={habitFormatted.how_much_achieved}
+          >
+            <div></div>
+          </InfoHowMuchAchieved>
+        </HabitInfo>
+        <ImageContainer category={habitFormatted.category} className="lottie">
+          {habitFormatted.categoryFormatted && (
+            <Lottie
+              options={habitFormatted.categoryFormatted?.image}
+              isPaused={paused}
+            />
+          )}
+        </ImageContainer>
+      </CardContainer>
+      <ModalCheckin
+        habit={habit}
+        isModalVisible={isModalVisible}
+        setIsModalVisible={setIsModalVisible}
+        habitFormatted={habitFormatted}
+      ></ModalCheckin>
+    </>
   );
 };
 
