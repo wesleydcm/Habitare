5a99f0d5b14ab2baf27430dfb6711fcd978e62d4
wesleydcm <wesleycostamatos@gmail.com>
2021-06-16 00:52:44 -0300

    feat:filter functionality on the groups page

--- src/pages/Groups/index.jsx
+++ src/pages/Groups/index.jsx
@@ -1,4 +1,4 @@
-import { useContext, useEffect } from "react";
+import { useContext, useEffect, useState } from "react";
 
 import { Redirect, useHistory, useLocation } from "react-router-dom";
 import Button from "../../components/Button";
@@ -29,6 +29,16 @@ const Groups = () => {
   const { pathname } = useLocation();
   const history = useHistory();
 
+  const [myGroups, setMyGroups] = useState(groupHabits);
+
+  const lastCategory = JSON.parse(
+    localStorage.getItem(`@Habitare:groupsLastCategory`)
+  );
+
+  const [allGroups, setAllGroups] = useState(
+    lastCategory === "displayAll" || lastCategory === null ? true : false
+  );
+
   useEffect(() => {
     if (pathname === "/groups") {
       loadGroupHabits();
@@ -48,13 +58,41 @@ const Groups = () => {
     // eslint-disable-next-line
   }, [render]);
 
+  useEffect(() => {
+    const lastCategory = JSON.parse(
+      localStorage.getItem(`@Habitare:groupsLastCategory`)
+    );
+
+    lastCategory === "displayAll"
+      ? setMyGroups(groupHabits)
+      : setMyGroups(
+          groupHabits.filter((habit) => habit.category === lastCategory)
+        );
+  }, [groupHabits]);
+
+  const handleFilter = (category) => {
+    if (category === "displayAll") {
+      setMyGroups(groupHabits);
+      setAllGroups(true);
+    } else {
+      const filteredHabits = groupHabits.filter(
+        (group) => group.category === category
+      );
+      setMyGroups(filteredHabits);
+      setAllGroups(false);
+    }
+  };
+
   if (!authenticated) {
     return <Redirect to="/login" />;
   }
   return (
     <GroupsContainer>
       <FiltersAndButtonsWrapper>
-        <FilterCategory></FilterCategory>
+        <FilterCategory
+          handleFilter={handleFilter}
+          page="groups"
+        ></FilterCategory>
         <ButtonsWrapper>
           <Button
             onClickFunc={() => history.push("/groups")}
@@ -83,9 +121,13 @@ const Groups = () => {
             </h3>
           ) : (
             <GroupsList>
-              {groupHabits.map((group) => {
-                return <GroupCard group={group} key={group.id} />;
-              })}
+              {allGroups
+                ? groupHabits.map((group) => {
+                    return <GroupCard group={group} key={group.id} />;
+                  })
+                : myGroups.map((group) => {
+                    return <GroupCard group={group} key={group.id} />;
+                  })}
             </GroupsList>
           )}
         </>
