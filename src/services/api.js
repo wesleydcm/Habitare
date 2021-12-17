2bcb8c7f2cd57ac322a0dcfca5d1070891f418f3
Wesley da Costa Matos <71963380+wesleydcm@users.noreply.github.com>
2021-12-16 23:59:01 -0300

    chore: update endpoint api

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://kabit-api.herokuapp.com/",
+  baseURL: "https://kenzie-habits.herokuapp.com/",
 });
 
 export default api;
